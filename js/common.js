54cfc98782f6f5388fee0aadfc0cd1b9bae661df
Sujin <kgj1212_@naver.com>
2020-08-07 01:04:32 +0900

    mm

--- js/common.js
+++ js/common.js
@@ -1,3 +1,33 @@
-// $(document).ready(function(){
+$(document).ready(function(){
+  $(window).on('resize', function() {
+    this.location.href = this.location.href;
+  });
 
-// });
+  mobile_swipe();
+
+  new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+});
+
+function mobile_swipe(){
+  var windowWidth = $( window ).width();
+  var wrapper = $('.content-point');
+  var list = $('.point-list');
+  var items = $('.point-item');
+
+  if(windowWidth < 416) {
+    // console.log('mobile screen')
+    $(wrapper).addClass('swiper-container');
+    $(list).addClass('swiper-wrapper');
+    $(items).addClass('swiper-slide');
+  } else {
+    // console.log('pc screen')
+    $(wrapper).removeClass('swiper-container');
+    $(list).removeClass('swiper-wrapper');
+    $(items).removeClass('swiper-slide');
+  }
+}
+  
